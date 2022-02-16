f6c02d057b34c36b9f9b6a08847f25bffd33a094
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-16 21:43:06 +0530

    Added:Calculate the length

--- src/com/linecomparison/oops/LineComparison.java
+++ src/com/linecomparison/oops/LineComparison.java
@@ -1,8 +1,31 @@
 package com.linecomparison.oops;
 
+import java.util.Scanner;
+
 public class LineComparison {
 	
-	public static void main(String[] args) {
-		 System.out.println("welcome to line comparison");
-	}
+		public static double lineValues() {
+			try (Scanner sc = new Scanner(System.in)) {
+				double w, x, y, z;
+
+				System.out.println("Enter Values For First Point");
+				w = sc.nextDouble();
+				y = sc.nextDouble();
+
+				System.out.println("Enter Values For Second Point");
+				x = sc.nextDouble();
+				z = sc.nextDouble();
+
+				double lineLength = (double) Math.sqrt(Math.pow((x - w), 2) + Math.pow((z - y), 2));
+				return lineLength;
+			}
+		}
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to line comparison ");
+			double lineLength = lineValues();
+			System.out.println("Length Of Line is: " + lineLength);
+
+		}
+	
 }
